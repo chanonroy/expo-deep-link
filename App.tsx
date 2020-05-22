31f0a2672c47899a5a11ba3d8e152f56f51fa6fd
Chanon Roy <chanonroy@gmail.com>
2020-05-22 08:24:41 +0100

    fix: resolve hot reloading

--- App.tsx
+++ App.tsx
@@ -1,62 +1,14 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
-import { NativeRouter, Route, Link } from "react-router-native";
-
-const Home = () => <Text style={styles.header}>Home</Text>;
-
-const About = () => <Text style={styles.header}>About</Text>;
-
+import One from "./components/One";
 
 class App extends React.Component {
   render() {
     return (
-      <NativeRouter>
-        <View style={styles.container}>
-          <View style={styles.nav}>
-            <Link to="/" underlayColor="#f0f4f7" style={styles.navItem}>
-              <Text>Home</Text>
-            </Link>
-            <Link to="/about" underlayColor="#f0f4f7" style={styles.navItem}>
-              <Text>About</Text>
-            </Link>
-            <Link to="/topics" underlayColor="#f0f4f7" style={styles.navItem}>
-              <Text>Topics</Text>
-            </Link>
-          </View>
-
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-        </View>
-      </NativeRouter>
+      <One />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 25,
-    padding: 10
-  },
-  header: {
-    fontSize: 20
-  },
-  nav: {
-    flexDirection: "row",
-    justifyContent: "space-around"
-  },
-  navItem: {
-    flex: 1,
-    alignItems: "center",
-    padding: 10
-  },
-  subNavItem: {
-    padding: 5
-  },
-  topic: {
-    textAlign: "center",
-    fontSize: 15
-  }
-});
+
 
 export default App;
